ed6fb44bae6e282acfc5f9a11fd9be7514a12a98
osxgirl <osxgirl@mac.com>
2021-07-28 22:54:18 -0400

    //💙

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import GameComponent from './App';
-
+import { Auth0Provider } from "@auth0/auth0-react";
+import App from './App';
 
 ReactDOM.render(
-  <GameComponent />,
+  <Auth0Provider
+    domain="dev-cpc56fcs.us.auth0.com"
+    clientId="HbyKHdGvLSzHQmxf4rs8tA0PIxvhZgqz"
+    redirectUri={window.location.origin}
+   >
+    <App />
+  </Auth0Provider>,
   document.getElementById('root')
+            
+                
 );
